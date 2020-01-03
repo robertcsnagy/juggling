0f323ca76d49a2395a7af1c9a5a57ec06931b3df
robertcnagy <robert.nagy@crowncommercial.gov.uk>
2020-01-03 16:39:59 +0000

    supplier proto

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Buyer account',
+  serviceName: 'Supplier account',
 
   // Default port that prototype runs on
   port: '3000',
